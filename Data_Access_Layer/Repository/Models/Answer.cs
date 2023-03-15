9e4c98224d86d9b86ad6cdfeb2e1d02ed29596f3
aklinar <b.negro@aubay.lu>
2023-03-15 16:55:15 +0100

    FIX - permutation

--- Data_Access_Layer/Repository/Models/Answer.cs
+++ Data_Access_Layer/Repository/Models/Answer.cs
@@ -7,17 +7,11 @@ using System.Threading.Tasks;
 
 namespace Data_Access_Layer.Repository.Models
 {
-    public class TestAttribution
+    public class Answer
     {
-        public int TestAttributionId { get; set; }
-        public int TestId { get; set; }
-        public int LogonId { get; set; }
-        public int LanguagesID { get; set; }
-        public int TestStatusId { get; set; }
-        public Boolean isWithTimer { get; set; }
-        public DateTime EndDate { get; set; }
-        public DateTime TestDate { get; set; }
+        public int AnswerId { get; set; }
         public string Comment { get; set; }
+        public Boolean isActive { get; set; }
         public Boolean isDeleted { get; set; }
     }
 }
