6de7ed436bf54ef34aa0f49f1ef2a4f9296b258d
aklinar <b.negro@aubay.lu>
2023-03-20 12:20:10 +0100

    UPD - REST FULL works

--- Business_Logic_Layer/Models/DomainComposeModel.cs
+++ Business_Logic_Layer/Models/DomainComposeModel.cs
@@ -8,6 +8,12 @@ namespace Business_Logic_Layer.Models
 {
     public class DomainComposeModel
     {
+        public DomainComposeModel() { }
+        public DomainComposeModel(DomainComposeModelPostDTO domainComposeModelPostDTO)
+        {
+            this.DomainId = domainComposeModelPostDTO.DomainId;
+            this.SubDomainid = domainComposeModelPostDTO.SubDomainid;
+        }
         public String DomainComposeId { get; set; }
         public string DomainId { get; set; }
         public string SubDomainid { get; set; }
--- SkillQuizWebApi/Controllers/DomainComposeController.cs
+++ SkillQuizWebApi/Controllers/DomainComposeController.cs
@@ -1,15 +1,19 @@
 ﻿using Business_Logic_Layer.Models;
+using Data_Access_Layer.Repository.Models;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
-
-
-
-
+using System.Reflection;
+using System.Text.Json.Nodes;
 
 namespace SkillQuizzWebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/v1/[controller]")]
     public class DomainComposeController : ControllerBase
     {
 
@@ -21,42 +25,103 @@ namespace SkillQuizzWebApi.Controllers
             _IDomainCompose = interfaceDomainCompose;
         }
 
-
+        //GET api/v1/DomainCompose
         [HttpGet]
-        [Route("getDomainsCompose")]
-
-
+        [Route("")]
         public List<DomainComposeModel> GetAllDomainCompose()
         {
             return _IDomainCompose.GetAllDomainCompose();
         }
 
 
-
+        //GET api/v1/DomainCompose/{id}
         [HttpGet]
-        [Route("getDomainCompose")]
+        [Route("{id:int}")]
         public ActionResult<DomainComposeModel> GetDomainComposeById(int id)
         {
-            var domaincompose = _IDomainCompose.GetDomainComposeById(id);
+            var domainCompose = _IDomainCompose.GetDomainComposeById(id);
 
-            if (domaincompose == null)
+            if (domainCompose == null)
             {
                 return NotFound("Invalid ID");
             }
 
-            return Ok(domaincompose);
+            return Ok(domainCompose);
         }
 
-
-
-
-        [Route("postDomainCompose")]
+        //POST api/v1/DomainCompose
         [HttpPost]
-        public void postDomainCompose([FromBody] DomainComposeModel domaincomposeModel)
+        [Route("")]
+        public ActionResult<DomainComposeModel> PostDomainCompose([FromBody] DomainComposeModelPostDTO domainComposeModelPostDTO)
         {
-            _IDomainCompose.PostDomainCompose(domaincomposeModel);
+            if (domainComposeModelPostDTO != null)
+            {
+                var domainComposeModel = new DomainComposeModel(domainComposeModelPostDTO);
+                var domainComposeResult = _IDomainCompose.PostDomainCompose(domainComposeModel);
+                if (domainComposeResult != null)
+                {
+                    return Created("/api/v1/DomainCompose/" + domainComposeModel.DomainComposeId, domainComposeResult);
+                }
+            }
+            return BadRequest(ModelState);
         }
 
+        //PATCH api/v1/DomainCompose/{id}
+        [HttpPatch]
+        [Route("{id:int}")]
+        public ActionResult<DomainComposeModel> PatchDomainCompose([FromRoute] int id, [FromBody] JsonPatchDocument<DomainCompose> domainComposeModelJSON)
+        {
+            if (domainComposeModelJSON != null)
+            {
+                var domainCompose = _IDomainCompose.PatchDomainCompose(id, domainComposeModelJSON);
+                return Ok(domainCompose);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        //PUT api/v1/DomainCompose
+        [HttpPut]
+        [Route("{id:int}")]
+        public ActionResult<DomainComposeModel> PatchDomainCompose([FromRoute] int id, [FromBody] DomainComposeModel domainComposeModel)
+        {
+            if (domainComposeModel.DomainComposeId != id.ToString())
+            {
+                return BadRequest("ID mismatch");
+            }
+            else
+                if (domainComposeModel != null)
+            {
+                var domainCompose = _IDomainCompose.PutDomainCompose(domainComposeModel);
+                return Ok(domainCompose);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        //DELETE api/v1/DomainCompose/{id}
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ActionResult<DomainComposeModel> DeleteDomainCompose([FromRoute] int id)
+        {
+            var domainCompose = _IDomainCompose.GetDomainComposeById(id);
+
+            if (domainCompose == null)
+            {
+                return NotFound("Invalid ID");
+            }
+            else
+            {
+                _IDomainCompose.DeleteDomainCompose(id);
+                return Ok(domainCompose);
+            }
+
+            //_IDomainCompose.DeleteDomainCompose(id);
+        }
 
 
         //(This is the bad practise!) = > this should instead also call the BLL 
