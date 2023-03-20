3e6487d2af1f38d6848b24a0da4e7f4ebf2dd0b0
aklinar <b.negro@aubay.lu>
2023-03-20 14:54:08 +0100

    UPD - Answer REST still to discuss

--- Business_Logic_Layer/AnswerBLL.cs
+++ Business_Logic_Layer/AnswerBLL.cs
@@ -4,18 +4,20 @@ using AutoMapper;
 using Business_Logic_Layer.Models;
 using Business_Logic_Layer.Interface;
 using Data_Access_Layer.Repository.Models;
+using Data_Access_Layer.DAL;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace Business_Logic_Layer
 {
     public class AnswerBLL : InterfaceAnswer
     {
 
-        private Data_Access_Layer.AnswerDAL _DAL;
+        private AnswerDAL _DAL;
         private Mapper _AnswerMapper;
 
         public AnswerBLL()
         {
-            _DAL = new Data_Access_Layer.AnswerDAL();
+            _DAL = new Data_Access_Layer.DAL.AnswerDAL();
             var _configAnswer = new MapperConfiguration(cfg => cfg.CreateMap<Answer, AnswerModel>().ReverseMap());
 
             _AnswerMapper = new Mapper(_configAnswer);
@@ -39,13 +41,33 @@ namespace Business_Logic_Layer
         }
 
 
-        public void PostAnswer(AnswerModel answerModel)
+        public void PostAnswer(int id)
         {
-            Answer answerEntity = _AnswerMapper.Map<AnswerModel, Answer>(answerModel);
-            _DAL.postAnswer(answerEntity);
+            /*var answerModel = new AnswerModel(id);
+            Answer answerEntity = _AnswerMapper.Map<AnswerModel, Answer>(answerModel);*/
         }
 
+
+        public AnswerModel PatchAnswer(int id, JsonPatchDocument<Answer> answerModelJSON)
+        {
+            var answerEntity = _DAL.PatchAnswer(id, answerModelJSON);
+
+            AnswerModel answerModel = _AnswerMapper.Map<Answer, AnswerModel>(answerEntity);
+
+            return answerModel;
+        }
+
+        public AnswerModel PutAnswer(AnswerModel answerModel)
+        {
+            Answer answerEntity = _AnswerMapper.Map<AnswerModel, Answer>(answerModel);
+            var answer = _DAL.PutAnswer(answerEntity);
+            AnswerModel answerModelReturn = _AnswerMapper.Map<Answer, AnswerModel>(answer);
+            return answerModelReturn;
+        }
+        public void DeleteAnswer(int id)
+        {
+            _DAL.DeleteAnswer(id);
+        }
     }
 }
-
 
--- SkillQuizWebApi/Controllers/AnswerController.cs
+++ SkillQuizWebApi/Controllers/AnswerController.cs
@@ -1,16 +1,19 @@
 ﻿using Business_Logic_Layer.Models;
 using Data_Access_Layer.Repository.Models;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
-
-
-
-
+using System.Reflection;
+using System.Text.Json.Nodes;
 
 namespace SkillQuizzWebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/v1/[controller]")]
     public class AnswerController : ControllerBase
     {
 
@@ -21,21 +24,19 @@ namespace SkillQuizzWebApi.Controllers
             _BLL = new Business_Logic_Layer.AnswerBLL();
             _IAnswer = interfaceAnswer;
         }
-
-
+        /*
+        //GET api/v1/Answer
         [HttpGet]
-        [Route("getAnswers")]
-
-
+        [Route("")]
         public List<AnswerModel> GetAllAnswer()
         {
             return _IAnswer.GetAllAnswer();
         }
 
 
-
+        //GET api/v1/Answer/{id}
         [HttpGet]
-        [Route("getAnswer")]
+        [Route("{id:int}")]
         public ActionResult<AnswerModel> GetAnswerById(int id)
         {
             var answer = _IAnswer.GetAnswerById(id);
@@ -47,18 +48,64 @@ namespace SkillQuizzWebApi.Controllers
 
             return Ok(answer);
         }
-
-
-
-
-        [Route("postAnswer")]
-        [HttpPost]
-        public void postAnswer([FromBody] AnswerModel answerModel)
+        
+        //PATCH api/v1/Answer/{id}
+        [HttpPatch]
+        [Route("{id:int}")]
+        public ActionResult<AnswerModel> PatchAnswer([FromRoute] int id, [FromBody] JsonPatchDocument<Answer> answerModelJSON)
         {
-            _IAnswer.PostAnswer(answerModel);
+            if (answerModelJSON != null)
+            {
+                var answer = _IAnswer.PatchAnswer(id, answerModelJSON);
+                return Ok(answer);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
         }
 
+        //PUT api/v1/Answer
+        [HttpPut]
+        [Route("{id:int}")]
+        public ActionResult<AnswerModel> PatchAnswer([FromRoute] int id, [FromBody] AnswerModel answerModel)
+        {
+            if (answerModel.AnswerId != id.ToString())
+            {
+                return BadRequest("ID mismatch");
+            }
+            else
+                if (answerModel != null)
+            {
+                var answer = _IAnswer.PutAnswer(answerModel);
+                return Ok(answer);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
 
+        //DELETE api/v1/Answer/{id}
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ActionResult<AnswerModel> DeleteAnswer([FromRoute] int id)
+        {
+            var answer = _IAnswer.GetAnswerById(id);
+
+            if (answer == null)
+            {
+                return NotFound("Invalid ID");
+            }
+            else
+            {
+                _IAnswer.DeleteAnswer(id);
+                return Ok(answer);
+            }
+
+            //_IAnswer.DeleteAnswer(id);
+        }
+        */
 
         //(This is the bad practise!) = > this should instead also call the BLL 
         //[Route("deletePerson")]
