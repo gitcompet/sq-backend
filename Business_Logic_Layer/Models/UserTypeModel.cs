a6772b26e7148a4b18cfa4434c10ba1e51349b3b
aklinar <b.negro@aubay.lu>
2023-03-22 15:36:23 +0100

    UPD - REST FULL works

--- Business_Logic_Layer/Models/UserTypeModel.cs
+++ Business_Logic_Layer/Models/UserTypeModel.cs
@@ -8,9 +8,23 @@ namespace Business_Logic_Layer.Models
 {
     public class UserTypeModel
     {
+        public UserTypeModel() { }
+        public UserTypeModel(UserTypeModelPostDTO userTypeModelPostDTO)
+        {
+            if (userTypeModelPostDTO.Description != null)
+            {
+                this.Description = userTypeModelPostDTO.Description;
+            }
+            else
+            {
+                this.Description = "";
+            }
+            this.isActive = true;
+            this.isDeleted = false;
+        }
         public String UserTypeId { get; set; }
         public string Description { get; set; }
-        public Boolean IsActive { get; set; }
-        public Boolean IsDeleted { get; set; }
+        public Boolean isActive { get; set; }
+        public Boolean isDeleted { get; set; }
     }
 }
