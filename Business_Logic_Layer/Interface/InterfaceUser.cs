3d4f65544e499a2cca2155ef9bd771339c31172b
aklinar <b.negro@aubay.lu>
2023-03-22 15:24:53 +0100

    UPD - REST FULL untested

--- Business_Logic_Layer/Interface/InterfaceUser.cs
+++ Business_Logic_Layer/Interface/InterfaceUser.cs
@@ -1,17 +1,22 @@
 ﻿using Business_Logic_Layer.Models;
+using Data_Access_Layer.Repository.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Business_Logic_Layer.Interface
 {
     public interface InterfaceUser
     {
+
         List<UserModel> GetAllUser();
         UserModel GetUserById(int id);
         UserModel GetUserByUsername(string username);
-        CreatedUserDTO PostUser(UserModelDTO userModelDTO);
+        UserModel PostUser(UserModel userModel);
+        UserModel PatchUser(int id, JsonPatchDocument<User> userModelJSON);
+        UserModel PutUser(UserModel userModel);
+        void DeleteUser(int id);
     }
 }
